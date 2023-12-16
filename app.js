8a928f7c3b43f2707cbe4a15ecc1fd478155ac4b
HoangDucBach <124645604+HoangDucBach@users.noreply.github.com>
2023-12-16 22:33:22 +0700

    update:

--- app.js
+++ app.js
@@ -19,10 +19,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fun)
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Header', 'Content-Type');
+  next();
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
